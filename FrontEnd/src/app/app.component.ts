1e530491857a2b0be2682fa16866edfe182af569
Gözde Sarıaslan <sariaslangozde@gmail.com>
2021-04-08 16:30:10 +0300

    product, category, navi components created

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'FrontEnd';
+  title: string = 'northwind';
+  user: string = 'Gözde Sarıaslan';
+  product1 = {
+    productId: 1,
+    productName: 'Bardak',
+    categoryId: 1,
+    unitPrice: 5,
+  };
+  product2 = {
+    productId: 2,
+    productName: 'Laptop',
+    categoryId: 1,
+    unitPrice: 5,
+  };
+  product3 = {
+    productId: 3,
+    productName: 'Mouse',
+    categoryId: 1,
+    unitPrice: 5,
+  };
+  product4 = {
+    productId: 4,
+    productName: 'Keyboard',
+    categoryId: 1,
+    unitPrice: 5,
+  };
+  product5 = {
+    productId: 5,
+    productName: 'Camera',
+    categoryId: 1,
+    unitPrice: 5,
+  };
 }
