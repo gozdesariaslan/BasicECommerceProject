85b7861c51ee3b15e55ad8edc4bb9cec0643d921
Gözde Sarıaslan <sariaslangozde@gmail.com>
2021-04-08 16:40:30 +0300

    navbar added, product component updated

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -8,34 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'northwind';
   user: string = 'Gözde Sarıaslan';
-  product1 = {
-    productId: 1,
-    productName: 'Bardak',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product2 = {
-    productId: 2,
-    productName: 'Laptop',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product3 = {
-    productId: 3,
-    productName: 'Mouse',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product4 = {
-    productId: 4,
-    productName: 'Keyboard',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product5 = {
-    productId: 5,
-    productName: 'Camera',
-    categoryId: 1,
-    unitPrice: 5,
-  };
+  
 }
