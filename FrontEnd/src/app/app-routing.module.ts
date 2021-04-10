5178a2031e2bb562c65f6f3631cfe35bc11e6b6c
Gözde Sarıaslan <sariaslangozde@gmail.com>
2021-04-10 16:21:36 +0300

    getproductsbycategory func added

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
-  {path:"", component:ProductComponent},
+  {path:"",pathMatch:"full", component:ProductComponent},
   {path:"products", component:ProductComponent},
   {path:"products/category/:categoryId", component:ProductComponent}
 ];
